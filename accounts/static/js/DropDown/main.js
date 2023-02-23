372c7ea19cd44f55eba9c589bf88625cc3e89485
RinorTheA <107106932+RinorTheA@users.noreply.github.com>
2023-02-23 11:53:14 +0100

    DropDownChange

---
+++ accounts/static/js/DropDown/main.js
@@ -0,0 +1,8 @@
+function toggleDropdownMenu() {
+  var dropdownMenu = document.getElementById('dropdownA');
+  if (dropdownMenu.style.display === 'block') {
+    dropdownMenu.style.display = 'none';
+  } else {
+    dropdownMenu.style.display = 'block';
+  }
+}
