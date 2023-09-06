e20791277bf3fa3fa28580296f7ab8cf79ac120c
RinorTheA <107106932+RinorTheA@users.noreply.github.com>
2023-09-06 12:06:04 +0200

    remove header register login

--- accounts/task.py
+++ accounts/task.py
@@ -7,7 +7,7 @@ from accounts.views import CalendlyAPI
 def my_task():
     CalendlyAPI()
     # Your task logic here
-    print("Hey There im Runnning from Sam!")
+
 
 scheduler.add_job(my_task, 'interval', hours=1)
 scheduler.start()
