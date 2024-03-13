ea6f8cccae27f251d64c4b7af59d1c0ab5d3bcfb
RinorTheA <107106932+RinorTheA@users.noreply.github.com>
2024-03-13 21:13:37 +0000

    changee

--- static/js/reserve/reserve-second-next.js
+++ static/js/reserve/reserve-second-next.js
@@ -1,5 +1,8 @@
 const inputFields = Array.from(document.querySelectorAll('.mutual-input'));
 const myButton = document.getElementById('next-button');
+const phoneNumber = document.getElementById("phone-input");
+const emailAddressInput = document.getElementById('id_email');
+const select_country_mutual = document.getElementById('id_country')
 var second_reserve_form_next = document.querySelector(".second-reserve-form");
 var third_reserve_form_next = document.querySelector(".third-reserve-form");
 var screening_question_div_1 = document.querySelector(".first-screening-question-div");
@@ -18,14 +21,142 @@ var myPaymentFunction = function() {
     reserve_form_next.style.display = "none";
 }
 
+var myDisableClickFunction = function() {
+  inputFields.forEach(input => {
+    const errorDivId = input.id + '-error-message';
+    let errorDiv = document.getElementById(errorDivId);
+
+    if (input.value.trim() === '') {
+      if (!errorDiv) {
+        errorDiv = document.createElement('div');
+        errorDiv.id = errorDivId;
+        errorDiv.classList.add('error-message');
+        errorDiv.style.color = "red";
+        errorDiv.style.fontSize = "14px";
+        errorDiv.style.marginTop = "4px";
+        input.parentNode.insertBefore(errorDiv, input.nextSibling);
+      } else {
+        input.style.border = "0.5px solid red";
+        input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+        errorDiv.textContent = 'This field is required';
+
+      }
+    } else {
+      if (input.id === 'id_email') {
+        const emailValue_second = input.value.trim();
+        const isEmailValid_second = /^[^\s@]+(\.[^\s@]+)*@[^\s@]+\.[^\s@]+$/.test(emailValue_second);
+
+        if (!isEmailValid_second) {
+          input.style.border = "0.5px solid red";
+          input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+          errorDiv.textContent = 'This email is invalid';
+        } else {
+        }
+      }
+      
+    }
+
+    if (input.id === "phone-input") {
+      if (input.value.length < 12) {
+        input.style.border = "0.5px solid red";
+        input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+        errorDiv.textContent = 'This field is incorrect';
+      }
+    }
+
+    input.addEventListener('focus', function() {
+      const errorDivId = input.id + '-error-message';
+      let errorDiv = document.getElementById(errorDivId);
+      input.style.border = "1px solid #1877f2";
+      input.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.1)";
+      errorDiv.textContent = '';
+    });
+
+    input.addEventListener('blur', function() {
+      const errorDivId = input.id + '-error-message';
+      let errorDiv = document.getElementById(errorDivId);
+      
+      if (!errorDiv) {
+        errorDiv = document.createElement('div');
+        errorDiv.id = errorDivId;
+        errorDiv.classList.add('error-message');
+        errorDiv.style.color = "red";
+        errorDiv.style.fontSize = "14px";
+        errorDiv.style.marginTop = "4px";
+        input.parentNode.insertBefore(errorDiv, input.nextSibling);
+        input.style.border = "0.5px solid red";
+        input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+        errorDiv.textContent = 'This field is required';
+    }
+      else {
+        input.style.border = "0.5px solid red";
+        input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+        errorDiv.textContent = 'This field is required';
+      }
+
+      if (input.id !== "id_email") {
+        if (input.value.trim() === "") {
+
+        }
+        else {
+          if (input.id !== "phone-input") {
+          input.style.border = "";
+          input.style.boxShadow = "";
+          errorDiv.textContent = '';
+          }
+          else {
+              if (input.value.length < 12) {
+                input.style.border = "0.5px solid red";
+                input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+                errorDiv.textContent = 'This field is incorrect';
+              }
+              else {
+                input.style.border = "";
+                input.style.boxShadow = "";
+                errorDiv.textContent = '';
+              }
+          }
+        }
+      } else {
+        const emailValue = input.value.trim();
+        const isEmailValid = /^[^\s@]+(\.[^\s@]+)*@[^\s@]+\.[^\s@]+$/.test(emailValue);
+        if (!isEmailValid) {
+          if (input.value.trim() !== "") {
+            input.style.border = "0.5px solid red";
+            input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+            errorDiv.textContent = 'This email is invalid';
+          } else {
+            input.style.border = "0.5px solid red";
+            input.style.boxShadow = "0 0 3px rgba(255, 0, 0, 0.5)";
+            errorDiv.textContent = 'This field is required';
+          }
+        } else {
+          input.style.border = "";
+          input.style.boxShadow = "";
+          errorDiv.textContent = '';
+        }
+      }
+    });
+
+
+
+    input.addEventListener('input', function() {
+      if (input !== emailAddressInput) {
+        // Update the value with the first letter uppercase
+        input.value = input.value.charAt(0).toUpperCase() + input.value.slice(1);
+      }
+    });
+  });
+};
+
+myDisableClickFunction();
 
 function checkInputs() {
   const emailInput = document.getElementById('id_email');
-  const phoneNumber = document.getElementById("phone-input");
   const emailValue = emailInput.value.trim();
   const isEmailValid = /^[^\s@]+(\.[^\s@]+)*@[^\s@]+\.[^\s@]+$/.test(emailValue);
   const allInputsFilled = inputFields.every(input => input.value.trim() !== '');
-  myButton.disabled = !allInputsFilled || !isEmailValid || phoneNumber.value.length < 6;
+  myButton.disabled = !allInputsFilled || !isEmailValid || phoneNumber.value.length < 12;
   
   
   if (myButton.disabled === false) {
@@ -38,10 +169,10 @@ function checkInputs() {
     screening_question_div.style.cursor = "pointer";
     screening_question_div.style.pointerEvents = "auto";
     screening_question_div.addEventListener("click", myClickFunction);
-
   }
   else {
     myButton.classList.remove("second-next-button");
+    myButton.addEventListener("click", myDisableClickFunction);
     myButton.removeEventListener("click", myClickFunction);
     screening_question_div.removeEventListener("click", myClickFunction);
     screening_question_div.style.pointerEvents = "none";
